37bca89fe90f8686f7e572333e9d4a7f446275ec
Mignon Joe <ynylxoxo@gmail.com>
2021-11-28 12:41:49 +0900

    Initial Commit

---
+++ EVIDD_Graphic/EVIDD_Graphic.h
@@ -0,0 +1,18 @@
+//
+//  EVIDD_Graphic.h
+//  EVIDD_Graphic
+//
+//  Created by 조민영 on 2021/11/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EVIDD_Graphic.
+FOUNDATION_EXPORT double EVIDD_GraphicVersionNumber;
+
+//! Project version string for EVIDD_Graphic.
+FOUNDATION_EXPORT const unsigned char EVIDD_GraphicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EVIDD_Graphic/PublicHeader.h>
+
+
